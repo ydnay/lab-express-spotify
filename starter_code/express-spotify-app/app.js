48396a7bbb53a6155f7f0be2b7f950c964e75f19
Yandy Gonzalez <yandygonzalez@gmail.com>
2018-06-14 17:24:39 -0400

    Setup Spotify API

---
+++ starter_code/express-spotify-app/app.js
@@ -0,0 +1,18 @@
+var SpotifyWebApi = require('spotify-web-api-node');
+
+// Remember to paste here your credentials
+var clientId = '29cfba169d6e4a2d8958a8cafe548047',
+    clientSecret = '27166424a7474908b7fedaa14d621d3d';
+
+var spotifyApi = new SpotifyWebApi({
+  clientId : clientId,
+  clientSecret : clientSecret
+});
+
+// Retrieve an access token.
+spotifyApi.clientCredentialsGrant()
+  .then(function(data) {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  }, function(err) {
+    console.log('Something went wrong when retrieving an access token', err);
+});
